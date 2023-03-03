eeb2d65d76400429d744a7d17600f16891d2f1a5
kyuki3rain <kyuki3rain@gmail.com>
2023-03-03 17:49:52 +0000

    もろもろの実装をguessで束ねる

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 mod factories;
+mod features;
 mod libs;
 mod models;
 
@@ -10,7 +11,7 @@ use clap::Parser;
 use factories::CodeFactory;
 use models::Possibility;
 
-use crate::models::Log;
+use crate::features::guess;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -51,28 +52,13 @@ fn main() {
         print!("{}桁の数字を入力してください: ", args.length);
         io::stdout().flush().unwrap();
 
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("入力エラー。read_line()で失敗しました。");
-
-        let guess = match factory.generate_from_str(&guess) {
-            Ok(guess) => guess,
+        let (log, is_correct) = match guess(&factory, &answer) {
+            Ok(r) => r,
             Err(e) => {
                 println!("{}\nもう一度入力してください。", e);
                 continue;
             }
         };
-
-        let (result, is_correct) = match answer.diff(&guess) {
-            Ok(d) => d,
-            Err(e) => {
-                println!("{}\nもう一度入力してください。", e);
-                continue;
-            }
-        };
-
-        let log = Log { guess, result };
 
         counter += 1;
         println!("{}", log);
