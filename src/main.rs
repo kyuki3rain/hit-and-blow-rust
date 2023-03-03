f6b08a90621cc925be716fe43139f73dd904e092
kyuki3rain <kyuki3rain@gmail.com>
2023-03-03 17:02:00 +0000

    remove guess feature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate rand;
 
 mod factories;
-mod features;
 mod libs;
 mod models;
 
@@ -11,7 +10,7 @@ use clap::Parser;
 use factories::CodeFactory;
 use models::Possibility;
 
-use crate::features::guess::guess;
+use crate::models::Log;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -52,13 +51,28 @@ fn main() {
         print!("{}桁の数字を入力してください: ", args.length);
         io::stdout().flush().unwrap();
 
-        let (log, is_correct) = match guess(&factory, &answer) {
-            Ok(r) => r,
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("入力エラー。read_line()で失敗しました。");
+
+        let guess = match factory.generate_from_str(&guess) {
+            Ok(guess) => guess,
             Err(e) => {
                 println!("{}\nもう一度入力してください。", e);
                 continue;
             }
         };
+
+        let (result, is_correct) = match answer.diff(&guess) {
+            Ok(d) => d,
+            Err(e) => {
+                println!("{}\nもう一度入力してください。", e);
+                continue;
+            }
+        };
+
+        let log = Log { guess, result };
 
         counter += 1;
         println!("{}", log);
